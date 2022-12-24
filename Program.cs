cde707a442f46b80f8b7a1335823df00896a4e9b
Artem Artemev <artem@artemev.su>
2022-12-24 17:03:48 +0300

    3d task commented out

--- Program.cs
+++ Program.cs
@@ -102,56 +102,56 @@
 // CheckOddIndArrayAndSumNumbers(arr);
 // PrintArrayWithOddInd(arr);
 
-void FillArray(double[] array)
-{
-    Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(1,99) + rnd.NextDouble();
-    }
-}
-double FindMinNumInArray(double[] array)
-{
-    double min = array[0];
-    for (int i = 1; i < array.Length; i++)
-    {
-        if (min > array[i])
-        {
-            min = array[i];
-        }
-    }
-    return min;
-}
-double FindMaxNumInArray(double[] array)
-{
-    double max = array[0];
-    for (int i = 1; i < array.Length; i++)
-    {
-        if (max < array[i])
-        {
-            max = array[i];
-        }
-    }
-    return max;
-}
-void PrintArray(double[] array) //Для себя, чтобы посмотреть весь массив
-{
-    int i = 0;
-    while (i < array.Length)
-    {
-        Console.Write(array[i] + " ");
-        i++;
-    }
-    Console.WriteLine();
-}
+// void FillArray(double[] array)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rnd.Next(1,99) + rnd.NextDouble();
+//     }
+// }
+// double FindMinNumInArray(double[] array)
+// {
+//     double min = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if (min > array[i])
+//         {
+//             min = array[i];
+//         }
+//     }
+//     return min;
+// }
+// double FindMaxNumInArray(double[] array)
+// {
+//     double max = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if (max < array[i])
+//         {
+//             max = array[i];
+//         }
+//     }
+//     return max;
+// }
+// void PrintArray(double[] array) //Для себя, чтобы посмотреть весь массив
+// {
+//     int i = 0;
+//     while (i < array.Length)
+//     {
+//         Console.Write(array[i] + " ");
+//         i++;
+//     }
+//     Console.WriteLine();
+// }
 
-Console.WriteLine("Сколько цифр в массиве должно быть? ");
-int a = Convert.ToInt32(Console.ReadLine());
-double[] arr = new double[a];
-FillArray(arr);
-double min = Math.Round(FindMinNumInArray(arr),3);
-double max = Math.Round(FindMaxNumInArray(arr),3);
-// PrintArray(arr);
-// Console.WriteLine($"минимальное число в массиве {min}");
-// Console.WriteLine($"максимальное число в массиве {max}");
-Console.WriteLine($"разница между {max} и {min} = {max - min}");
+// Console.WriteLine("Сколько цифр в массиве должно быть? ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// double[] arr = new double[a];
+// FillArray(arr);
+// double min = Math.Round(FindMinNumInArray(arr),3);
+// double max = Math.Round(FindMaxNumInArray(arr),3);
+// // PrintArray(arr);
+// // Console.WriteLine($"минимальное число в массиве {min}");
+// // Console.WriteLine($"максимальное число в массиве {max}");
+// Console.WriteLine($"разница между {max} и {min} = {max - min}");
