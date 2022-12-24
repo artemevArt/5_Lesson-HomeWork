e33eb6ff14a84ab183cf6c9d1f68d830fa44ba26
Artem Artemev <artem@artemev.su>
2022-12-24 17:00:17 +0300

    1st task done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+}
+void HowManyEvenNumArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+void PrintArray(int[] array) //Для себя, чтобы посмотреть весь массив
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write(array[i] + " ");
+        i++;
+    }
+    Console.WriteLine();
+}
+void PrintNumWithEvenIn(int[] array)//Для себя, чтобы посмотреть на четные числа
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.Write(array[i] + " ");
+        }
+        i++;
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Сколько цифр в массиве должно быть? ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[a];
+FillArray(arr);
+PrintArray(arr);
+HowManyEvenNumArray(arr);
+PrintNumWithEvenIn(arr);
